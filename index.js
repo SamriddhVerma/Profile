57913a084eb37a4b38cadd3bf649958889b356ec
SamriddhVerma <132701035+SamriddhVerma@users.noreply.github.com>
2023-11-01 14:00:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function badge()
+{
+    var li=document.getElementById('profile');
+    var bad=document.getElementById('badgez');
+    setTimeout(function() {bad.setAttribute("style","");}, 30);
+    setTimeout(function() {bad.classList.add('slide-in');}, 10);
+    setTimeout(function() {bad.classList.remove('slide-in');}, 700);
+    li.setAttribute("style","display:none");
+}
+
+function profile()
+{
+    var li=document.getElementById('profile');
+    var bad=document.getElementById('badgez');
+    setTimeout(function() {li.setAttribute("style","");}, 40);
+    setTimeout(function() {li.classList.add('slide-in');}, 10);
+    setTimeout(function() {li.classList.remove('slide-in');}, 700);
+    bad.setAttribute("style","display:none");
+}
