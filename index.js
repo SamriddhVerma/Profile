dddd8cd2f87bf88f2233d63f8cc9e8c24c429e13
Samriddh Verma <samriddhorsam@gmail.com>
2023-11-15 20:41:06 +0530

    fixed animations for mobile view

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function ()
 {
     var li=document.getElementById('profile');
-    setTimeout(function() {li.classList.remove('slide-in');}, 700);
+    setTimeout(function() {li.classList.remove('slide-in-right');}, 700);
 })
 function profile()
 {
     var li=document.getElementById('profile');
     document.getElementById('links').scrollIntoView({behavior: "smooth"});
-    setTimeout(function() {li.classList.add('slide-in');}, 30);
-    setTimeout(function() {li.classList.remove('slide-in');}, 700);
+    setTimeout(function() {li.classList.add('slide-in-right');}, 30);
+    setTimeout(function() {li.classList.remove('slide-in-right');}, 700);
 }
 function badge()
 {
     var bad=document.getElementById('badgez');
     document.getElementById('badges').scrollIntoView({behavior: "smooth"});
-    setTimeout(function() {bad.classList.add('slide-in');}, 30);
-    setTimeout(function() {bad.classList.remove('slide-in');}, 700);
+    setTimeout(function() {bad.classList.add('slide-in-left');}, 30);
+    setTimeout(function() {bad.classList.remove('slide-in-left');}, 700);
 }
