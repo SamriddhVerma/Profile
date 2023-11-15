6ecc88960125e779a117033b06e35594ab3a4b9a
Samriddh Verma <samriddhorsam@gmail.com>
2023-11-15 17:11:14 +0530

    Added animations

--- index.js
+++ index.js
@@ -2,27 +2,22 @@ document.addEventListener("DOMContentLoaded", function ()
 {
     var li=document.getElementById('profile');
     var bad=document.getElementById('badgez');
-    li.setAttribute("style","");
     setTimeout(function() {li.classList.add('slide-in');}, 10);
     setTimeout(function() {li.classList.remove('slide-in');}, 700);
-    bad.setAttribute("style","display:none");
 })
-function badge()
-{
-    var li=document.getElementById('profile');
-    var bad=document.getElementById('badgez');
-    setTimeout(function() {bad.setAttribute("style","");}, 30);
-    setTimeout(function() {bad.classList.add('slide-in');}, 10);
-    setTimeout(function() {bad.classList.remove('slide-in');}, 700);
-    li.setAttribute("style","display:none");
-}
-
 function profile()
 {
     var li=document.getElementById('profile');
     var bad=document.getElementById('badgez');
-    setTimeout(function() {li.setAttribute("style","");}, 40);
-    setTimeout(function() {li.classList.add('slide-in');}, 10);
+    document.getElementById('links').scrollIntoView({behavior: "smooth"});
+    setTimeout(function() {li.classList.add('slide-in');}, 30);
     setTimeout(function() {li.classList.remove('slide-in');}, 700);
-    bad.setAttribute("style","display:none");
+}
+function badge()
+{
+    var li=document.getElementById('profile');
+    var bad=document.getElementById('badgez');
+    document.getElementById('badges').scrollIntoView({behavior: "smooth"});
+    setTimeout(function() {bad.classList.add('slide-in');}, 30);
+    setTimeout(function() {bad.classList.remove('slide-in');}, 700);
 }
