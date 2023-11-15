054d05f128c04116bcff60971c16db40ea944417
Samriddh Verma <samriddhorsam@gmail.com>
2023-11-15 19:08:11 +0530

    opitimise

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 document.addEventListener("DOMContentLoaded", function ()
 {
     var li=document.getElementById('profile');
-    var bad=document.getElementById('badgez');
-    setTimeout(function() {li.classList.add('slide-in');}, 10);
     setTimeout(function() {li.classList.remove('slide-in');}, 700);
 })
 function profile()
 {
     var li=document.getElementById('profile');
-    var bad=document.getElementById('badgez');
     document.getElementById('links').scrollIntoView({behavior: "smooth"});
     setTimeout(function() {li.classList.add('slide-in');}, 30);
     setTimeout(function() {li.classList.remove('slide-in');}, 700);
 }
 function badge()
 {
-    var li=document.getElementById('profile');
     var bad=document.getElementById('badgez');
     document.getElementById('badges').scrollIntoView({behavior: "smooth"});
     setTimeout(function() {bad.classList.add('slide-in');}, 30);
